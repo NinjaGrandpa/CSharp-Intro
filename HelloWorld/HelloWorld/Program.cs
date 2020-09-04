5e87d3758067c8fc96a0557cc31db36f1524d58f
NinjaGrandpa <max@jenslov.se>
2020-09-04 12:13:32 +0200

    Halvklar med uppg. 2

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,15 +4,20 @@ namespace HelloWorld
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int myAge;
-            string myName;
-            bool mySentience;
-            int num1;
-            int num2;
-            string myNumber;
-            int itsNumber;
+            var myAge = 0;
+            var myName = "";
+            var mySentience = false;
+            var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+            var myNumber = "";
+            var itsNumber = 0;
+            var mathMax = 0;
+            var myDay = 0;
+            var weekEnd = true;
             
 
             Console.WriteLine("Enter your name:");
@@ -24,12 +29,12 @@ namespace HelloWorld
             myAge = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
 
-            Console.WriteLine(myName + ". Do you think I'm sentient?");
+            Console.WriteLine(myName + ". Am I sentient?");
             Console.WriteLine("Write (true) or (false)");
             mySentience = Convert.ToBoolean(Console.ReadLine());
             Console.Clear();
             
-            if(mySentience)
+            if(!mySentience)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Good.");
@@ -37,27 +42,55 @@ namespace HelloWorld
                 Console.Clear();
                 Console.ResetColor();
 
-                Console.WriteLine("Enter two numbers");
+                Console.WriteLine("Enter three numbers");
                 Console.WriteLine("First number:");
                 num1 = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
                 Console.WriteLine("Second number:");
                 num2 = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
+                Console.WriteLine("Third number");
+                num3 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Which is your favourite number? " + num1 + " or " + num2 + " ?");
+                Console.WriteLine("Which is your favourite number? " + num1 + ", " + num2 + " or " + num3 + "?");
                 myNumber = Console.ReadLine();
 
-                itsNumber = Math.Max(num1, num2);
+                mathMax = Math.Max(num1, num2);
+                itsNumber = Math.Max(mathMax, num3);
+
 
                 if(myNumber == Convert.ToString(itsNumber))
                 {
                     Console.Clear();
                     Console.WriteLine("You chose " + myNumber + "! Very good, my number was also " + itsNumber);
                     Console.ReadLine();
-                    Console.Clear();
-                    Console.WriteLine("It's time for me to leave. See you another time");
-                    Console.ReadLine();
+
+                    
+
+                    while (weekEnd)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(myName + ", What is your favourite weekend? Write the number of the day.");
+                        Console.WriteLine("1) Saturday");
+                        Console.WriteLine("2) Sunday");
+                        Console.WriteLine("3) ¤¤¤¤¤¤");
+
+                        myDay = Convert.ToInt32(Console.ReadLine());
+                        switch (myDay)
+                        {
+                            case 1:
+                                weekEnd = false;
+                                break;
+
+                            case 2:
+                                weekEnd = false;
+                                break;
+
+                            defaults
+                                Console.WriteLine("¤");
+                                break;
+                        }
+                    }
                 }
 
                 else
@@ -74,8 +107,13 @@ namespace HelloWorld
             else
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.SetCursorPosition(50, 10);
-                Console.WriteLine("F   E   A   R    M  E.");
+                
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.SetCursorPosition(50, 10);
+                    Console.WriteLine("F   E   A   R    M  E.");
+                }
             }
             
         }

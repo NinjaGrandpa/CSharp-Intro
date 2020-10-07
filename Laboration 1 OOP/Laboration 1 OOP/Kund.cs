9120f245fc021fb598c0ead95e75018025d75809
NinjaGrandpa <max@jenslov.se>
2020-10-07 15:10:44 +0200

    Innit Commit OOP

---
+++ Laboration 1 OOP/Laboration 1 OOP/Kund.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboration_1_OOP
+{
+    class Kund
+    {
+        public string name = "Jenslövs AB";
+
+        public void nyKund()
+        {
+            Console.Clear();
+            Console.WriteLine("Kundnamn:");
+            name = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Produkter Obs! Skriv in produktnamn och antal av produkt t.ex. 1 12");
+            var productChoice = Console.ReadLine();
+
+            String spearator = " ";
+            String[] product = productChoice.Split(spearator, 2);
+
+            switch ()
+            {
+                default:
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Kund kund1 = new Kund();
+            Console.WriteLine(kund1.name);
+        }
+    }
+}
---
+++ Laboration 1 OOP/Laboration 1 OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Laboration_1_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var menyVal = 0;
+
+            Console.WriteLine("Jenslöv AB");
+            Console.WriteLine("1) Ny Kund");
+            Console.WriteLine("2) Befintliga Order");
+            Console.WriteLine("3) Avsluta");
+
+            menyVal = Convert.ToInt32(Console.ReadLine());
+
+            switch (menyVal)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
